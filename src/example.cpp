bb939457b488bad3f9d46b88f1a98b4b1215e6b8
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-17 18:39:07 +0200

    remove bin

--- src/example.cpp
+++ src/example.cpp
@@ -78,18 +78,12 @@ int main2()
 
 	{
 		// equivalent ways of create A
-		std::shared_ptr<Base> a1 = factory.create_specialized<A>("first parameter", 2);
-		std::shared_ptr<A> a2 = factory.create_specialized<A>("first parameter", 2);
-		std::shared_ptr<Base> a3 = factory.create("A", "first parameter", 2);
+		auto a3 = factory.create("A", "first parameter", 2);
 
 		// equivalent ways of create B
-		std::shared_ptr<Base> b1 = factory.create_specialized<B>("first parameter", 2);
-		std::shared_ptr<B> b2 = factory.create_specialized<B>("first parameter", 2);
-		std::shared_ptr<Base> b3 = factory.create("B", "first parameter", 2);
+		auto b3 = factory.create("B", "first parameter", 2);
 
-		assert(a1 != a2);
-		assert(a3 != b1);
-		assert(b1 != b2);
+		assert(a3 != b3);
 	}
 
 	return(0);
