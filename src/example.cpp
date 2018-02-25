16b9b73a3a616a007db1a7151d3d9610f78ba6ff
Ricardo Marmolejo García <makiolo@gmail.com>
2018-02-25 21:35:36 +0100

    using cppunix

--- src/example.cpp
+++ src/example.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <string>
+#include <cppunix/parallel_scheduler.h>
+#include <cppunix/channel.h>
+#include <cppunix/shell.h>
 
 int main()
 {
-	std::cout << "hello world" << std::endl;
+	cu::parallel_scheduler sch;
+
+	cu::channel<std::string> command(sch);
+
+	command.pipeline(cu::run(), cu::strip());
+	
+	sch.spawn([&](auto& yield) {
+		command(yield, "ls -ltr");
+		command.close(yield);
+	});
+	sch.spawn([&](auto& yield) {
+		for(auto& e : cu::range(yield, command))
+		{
+			std::cout << "line: " << e << std::endl;
+		}
+	});
+	sch.run_until_complete();
 }
+
